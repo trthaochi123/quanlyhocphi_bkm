b94dd1ac901ff9997ce8236e5835d495d6326427
trthaochi123 <89992692+trthaochi123@users.noreply.github.com>
2024-07-24 10:03:14 +0700

    Update dashboard.blade.php

--- resources/views/receipts/dashboard.blade.php
+++ resources/views/receipts/dashboard.blade.php
@@ -18,15 +18,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-
     <link rel="stylesheet" href="{{ asset('assets/vendors/iconly/bold.css') }}">
-
     <link rel="stylesheet" href="{{ asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.svg" type="image/x-icon') }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('image/favicon.png') }}"/>
     <title>Thống Kê - Kế Toán</title>
 </head>
 
