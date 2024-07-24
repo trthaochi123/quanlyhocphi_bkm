96219bb2be2dc6cdd188980ce4d94ef3e1463eec
trthaochi123 <89992692+trthaochi123@users.noreply.github.com>
2024-07-24 10:05:27 +0700

    Update index.blade.php

--- resources/views/scholarships/index.blade.php
+++ resources/views/scholarships/index.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/admins.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/majors_fix.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/create.css') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('image/favicon.png') }}"/>
     <title>Danh Sách Mức Học Bổng</title>
     <style>
         #success-message {
